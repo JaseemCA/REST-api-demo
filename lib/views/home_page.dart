476f169907d1215aeec85894bce11748f540d0bd
JaseemCA <147582494+JaseemCA@users.noreply.github.com>
2024-05-27 16:40:20 +0530

    REST api

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
@@ -9,6 +11,8 @@ class Homepage extends StatefulWidget {
 }
 
 class _HomepageState extends State<Homepage> {
+  List<dynamic> users = [];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,6 +21,22 @@ class _HomepageState extends State<Homepage> {
         centerTitle: true,
         title: const Text('user details'),
       ),
+      body: ListView.builder(
+          itemCount: users.length,
+          itemBuilder: (context, index) {
+            final user = users[index];
+            final email = user['email'];
+            final name = user['name']['first'];
+            final imageurl = user['picture']['thumbnail'];
+
+            return ListTile(
+              leading: ClipRRect(
+                borderRadius: BorderRadius.circular(100),
+                child: Image.network(imageurl)),
+              title: Text(name.toString()),
+              subtitle: Text(email),
+            );
+          }),
       floatingActionButton: FloatingActionButton(
         onPressed: fetchUsers,
         backgroundColor: Colors.amber,
@@ -24,11 +44,17 @@ class _HomepageState extends State<Homepage> {
     );
   }
 
-  void fetchUsers() {
+  void fetchUsers() async {
     // print('user fetched');
-    const url = '';
+    const url = 'https://randomuser.me/api/?results=15';
     final uri = Uri.parse(url);
     //uniformResourseIdentifier=uri
-    http.get(uri);
+    final response = await http.get(uri);
+    final body = response.body;
+    final json = jsonDecode(body);
+    setState(() {
+      users = json['results'];
+    });
+    // print('fetchUser complete');
   }
 }
